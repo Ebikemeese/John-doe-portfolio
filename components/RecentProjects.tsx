19b9665db7da4dd7f59f9b70ed7e96cd8d1b51c2
Ebikemeese <ebikemeese257@gmail.com>
2025-09-19 07:35:05 +0100

    fixed project image height

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,7 +16,7 @@ const RecentProjects = () => {
                     <PinContainer title={link} href={link}>
                         <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
                             <div 
-                                className='relative w-full h-fit overflow-hidden lg:rounded-3xl'
+                                className='relative w-full h-full overflow-hidden lg:rounded-3xl'
                                 style={{ backgroundColor: "#13162D" }}
                             >
                                 <img src="/John-doe-portfolio/bg.png" alt="bg img" />
